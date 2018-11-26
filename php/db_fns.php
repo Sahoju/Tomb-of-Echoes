90651109cb29d1c16f86dce5a9b02076b2cdfae4
Sahoju <35224417+Sahoju@users.noreply.github.com>
2018-11-26 12:38:07 +0200

    Add files via upload

--- php/db_fns.php
+++ php/db_fns.php
@@ -19,15 +19,15 @@
         public function getLocations() {
             $loc = array(9);
             $locthree = array(3);
-
+            
             for($i = 0; $i < 3; $i++) {
-                $locthree[0] = rand(0,8);
+                $locthree[0] = array(rand(0,2), rand(0,2), '');
                 // check for duplicates
                 do {
-                    $locthree[1] = rand(0,8);
+                    $locthree[1] = array(rand(0,2), rand(0,2), '');
                 } while($locthree[0] == $locthree[1]);
                 do {
-                    $locthree[2] = rand(0,8);
+                    $locthree[2] = array(rand(0,2), rand(0,2), '');
                 } while($locthree[2] == $locthree[0] OR $locthree[2] == $locthree[1]);
 
                 /*
@@ -52,7 +52,7 @@
             $text = array(9);
 
             for($i = 0; $i < 9; $i++) {
-                $sql = 'SELECT content FROM writing WHERE location = ' . $loc[$i];
+                $sql = 'SELECT content FROM writing WHERE locx = ' . $loc[$i][0] . ' AND locy = ' . $loc[$i][1];
                 $result = $this->conn->query($sql);
 
                 // choose randomly a text from the MySQL query
@@ -71,16 +71,16 @@
             
             $texts = array();
             
-            // the array's key consists of phase and location
-            // i.e. 05 = phase 1, location 5
-            // i.e. 22 = phase 3, location 2
+            // the array's key consists of phase, locx and locy
+            // i.e. 001 = phase 1, locx 0, locy 1
+            // i.e. 222 = phase 3, locx 2, locy 2
             for($i = 0; $i < 9; $i++) {
                 if($i < 3) {
-                    $texts['0' . (string)$loc[$i]] = $text[$i];
+                    $texts['0' . (string)$loc[$i][0] . (string)$loc[$i][1]] = $text[$i];
                 } else if($i < 6) {
-                    $texts['1' . (string)$loc[$i]] = $text[$i];
+                    $texts['1' . (string)$loc[$i][0] . (string)$loc[$i][1]] = $text[$i];
                 } else {
-                    $texts['2' . (string)$loc[$i]] = $text[$i];
+                    $texts['2' . (string)$loc[$i][0] . (string)$loc[$i][1]] = $text[$i];
                 }
             }
 
