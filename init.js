efeef5f4bcb685dce3f3d201ec8ad4379ebc6d95
h3nkka <K8948@student.jamk.fi>
2018-11-27 05:59:21 +0200

    added text functionality, improved code

--- init.js
+++ init.js
@@ -1,5 +1,6 @@
-let imageElements = []; //for storing preloaded images in
-let texts = []; // for storing texts in
+let imageElements = []; //associative array for storing preloaded images in
+let textCoordinates = []; // for storing database-fetched randomly selected text (and audio) and their coordinates in
+let texts = []; //for storing local pre-defined text options in 
 
 function preloadImage(url) { //does the actual preloading by loading the images into the global cache
     var image = new Image();
@@ -7,59 +8,69 @@ function preloadImage(url) { //does the actual preloading by loading the images 
     return image;
 }
 
-$.ajax({ //preloading image files so that they don't have to be downloaded separately during gameplay
-    type: 'POST',
-    url: 'php/loadfiles.php',
-    cache: false,
-    success: function(result) {
-        if(result) {
-            let resultObj = eval(result);
-            console.log(resultObj);
-            preload(resultObj);
-            $('#percent').append("<input type='button' value='Start' onclick='startGame()'>");
-        } else {
-            console.log('error');
-        }
-    }
-});
+$(document).ready(function() { //collective function for preloading everything
 
-$.ajax({ //preloading image files so that they don't have to be downloaded separately during gameplay
-    type: 'POST',
-    url: 'php/getmessages.php',
-    dataType: 'json',
-    cache: false,
-    success: function(result) {
-        if(result) {
-            let resultObj = eval(result);
-            console.log('getmessages:');
-            console.log(resultObj);
-            $(resultObj).each(function() {
-                texts.push(this);
-            });
-            console.log(texts);
-        } else {
-            console.log('error');
+    $.ajax({ //preloading image files so that they don't have to be downloaded separately during gameplay
+        type: 'POST',
+        url: 'php/loadfiles.php',
+        cache: false,
+        success: function(result) {
+            if(result) {
+                let resultObj = eval(result);
+                console.log(resultObj);
+                if(resultObj.length > 2) //making sure that there are actually images to preload
+                    preload(resultObj);
+                preloadMessages();              
+            } else {
+                console.log('Error while fetching image files.');
+            }
         }
-    }
+    });
+
+    function preloadMessages() {
+        $.ajax({ //preloading text files so that they don't have to be downloaded separately during gameplay
+        type: 'POST',
+        url: 'php/getmessages.php',
+        dataType: 'json',
+        cache: false,
+        success: function(result) {
+                if(result) {
+                    let resultObj = eval(result);
+                    console.log('getmessages:');
+                    console.log(resultObj);           
+                    textCoordinates = resultObj;
+
+                    $.get('texts.txt', function(data){ //fetching options for in game text from local txt file
+                        texts = data.split('\n');
+                        console.log(texts);
+                    });
+
+                    //indicating that things are done and we're ready to start
+                    $('#percent').text(' 100 %');
+                    $('#percent').append("<input type='button' value='Start' onclick='startGame()'>");
+
+                } else {
+                    console.log('Error while fetching messages.');
+                }
+            }
+        });
+    } 
 });
 
 function preload(imgs) {
-    // skipdots: for some reason, in the json, there are "." and ".." before other images. this is to skip them
-    let skipdots = 1;
+    // for some reason, in the json, there are "." and ".." before other images. this is to skip them
+    imgs = imgs.slice(2);
     let progress = 0;
     $('#percent').text('0 %');
+    let imgCount = imgs.length - 1; //accommodating for 0-based array and progress division
+    console.log(imgCount);
     
-    $(imgs).each(function() {
-        if (skipdots > 2) {
-            //$('<img/>')[0].src = "img/";
-            imageElements.push(preloadImage("img/" + this));
-            progress += 1/41*100; //progress bar indicator
-            $('#percent').text(progress + ' %');
-        } else {
-            skipdots++;
-        }
+    $(imgs).each(function(i) {      
+        imageElements[this] = preloadImage("img/" + this); //inserts image objects into the associative array
+        progress += Math.round(i/imgCount); //progress bar indicator
+        $('#percent').text(progress + ' %');
     });
-    $('#percent').text(' 100 %');
+
     console.log('imageElements: ');
     console.log(imageElements);
 }
--- js/game.js
+++ js/game.js
@@ -1,44 +1,36 @@
 var player;
 var tileArray;
+var phase; //for keeping track of the game's phase which determines text locations
+var pC; //for keeping track of the player's coordinates
 
 
 function startGame() //setting the game up
 {  
     $('#percent').remove(); //removing progress bar
     $('#textWindow').css('visibility','visible'); //making the textWindow work
-    $('#textWindow').toggle(); //turning it off at first
+    $('#textWindow').toggle(); //turning it off at first  
+    $('#controls img').not('#inspect').css('visibility','visible'); //displaying arrow key GUI controls, but not the inspect button
     //welcomeUser();
 
-    $('#1strow td:nth-child(2)').append( //coupling UI controls with their respective images (arrows)
-        $('<img>').attr({src: imageElements[0].src,
-                        width: 50,
-                        height: 50,
-                        id: 'arr0'}));
-
-    for (i = 1; i < 4; i++) //arrow controls are arranged in table cells
-    {
-        $('#2ndrow td:nth-child(' + i + ')').append(
-            $('<img>').attr({src: imageElements[i].src,
-                            width: 50,
-                            height: 50,
-                            id: 'arr' + i }));
-    }
-
     //event listeners for player GUI arrows and keyboard arrows, moving and turning
-    $('#arr0').on('click', function(){
+    $('#arr1').on('click', function(){
         movePlayer(player.pDirection);       
     });
-    $('#arr1').on('click', function(){
+    $('#arr2').on('click', function(){
         turnPlayer(3);
     });
-    $('#arr2').on('click', function(){
+    $('#arr3').on('click', function(){
         movePlayer((player.pDirection + 2) % 4); //essentially flips the movement vector if the player is walking backwards
     });
-    $('#arr3').on('click', function(){
+    $('#arr4').on('click', function(){
         turnPlayer(1);
     });
-    $(window).keydown(function(event) {
+    $('#inspect').on('click', function(){ //event listener for inspecting tile text
+        if ($('#textWindow').css('display') == 'none')
         showText();
+    });
+    $(window).keydown(function(event) {
+        //showText();
         if(event.key == "ArrowUp") //making sure the pressed key was an arrow... apparently event.which is deprecated
             movePlayer(player.pDirection);
         else if (event.key == "ArrowLeft")
@@ -48,38 +40,29 @@ function startGame() //setting the game up
         else if (event.key == "ArrowDown")
             movePlayer((player.pDirection + 2) % 4);
     })
-    $('#everything').click(function() { //hiding text window when clicking out of it
+    $('#everything').click(function(event) { //hiding text window when clicking out of it
+        if(event.target !== event.currentTarget) return; //Do nothing if #everything was not directly clicked
         if ($('#textWindow').is(':visible'))
             hideText();
     })
- 
-    imageElements = imageElements.splice(4); //removing UI elements from image array, leaving only backgrounds
+
 
     player = new Player("test"); //creating a Player object with name, includes methods for turning and moving
     tileArray = [[],[],[]]; //2D array for storing the grid tiles in
+    phase = 0; //initializing game phase, varies between 0, 1 and 2
+
     // *** Initializing tiles with picture data ***
-    tileArray[0][0] = new Tile(imageElements[9],imageElements[0],imageElements[2],imageElements[10]);
-    tileArray[1][0] = new Tile(imageElements[10],imageElements[3],imageElements[1],imageElements[4]);
-    tileArray[2][0] = new Tile(imageElements[9],imageElements[10],imageElements[0],imageElements[2]);
-    tileArray[0][1] = new Tile(imageElements[3],imageElements[1],imageElements[4],imageElements[9]);
-    tileArray[1][1] = new Tile(imageElements[5],imageElements[6],imageElements[7],imageElements[8]);
-    tileArray[2][1] = new Tile(imageElements[4],imageElements[9],imageElements[3],imageElements[1]);
-    tileArray[0][2] = new Tile(imageElements[0],imageElements[2],imageElements[9],imageElements[10]);
-    tileArray[1][2] = new Tile(imageElements[1],imageElements[4],imageElements[10],imageElements[3]);
-    tileArray[2][2] = new Tile(imageElements[2],imageElements[10],imageElements[9],imageElements[0]);
-        
-    console.log(imageElements);
+    tileArray[0][0] = new Tile(imageElements["bg09.png"],imageElements["bg00.png"],imageElements["bg02.png"],imageElements["bg10.png"]);
+    tileArray[1][0] = new Tile(imageElements["bg10.png"],imageElements["bg03.png"],imageElements["bg01.png"],imageElements["bg04.png"]);
+    tileArray[2][0] = new Tile(imageElements["bg09.png"],imageElements["bg10.png"],imageElements["bg00.png"],imageElements["bg02.png"]);
+    tileArray[0][1] = new Tile(imageElements["bg03.png"],imageElements["bg01.png"],imageElements["bg04.png"],imageElements["bg09.png"]);
+    tileArray[1][1] = new Tile(imageElements["bg05.png"],imageElements["bg06.png"],imageElements["bg07.png"],imageElements["bg08.png"]);
+    tileArray[2][1] = new Tile(imageElements["bg04.png"],imageElements["bg09.png"],imageElements["bg03.png"],imageElements["bg01.png"]);
+    tileArray[0][2] = new Tile(imageElements["bg00.png"],imageElements["bg02.png"],imageElements["bg09.png"],imageElements["bg10.png"]);
+    tileArray[1][2] = new Tile(imageElements["bg01.png"],imageElements["bg04.png"],imageElements["bg10.png"],imageElements["bg03.png"]);
+    tileArray[2][2] = new Tile(imageElements["bg02.png"],imageElements["bg10.png"],imageElements["bg09.png"],imageElements["bg00.png"]); 
+
     $('#everything').css('background-image', 'url(' + tileArray[0][0].viewArray[1].src + ')'); //player starting position view
-}
-
-function showText() //displays the text window in game
-{
-    $('#textWindow').fadeIn();
-}
-
-function hideText() //hides the text window in game
-{
-    $('#textWindow').fadeOut();
 }
 
 function welcomeUser() //initial text popup that asks for the user's name (WIP)
@@ -89,8 +72,36 @@ function welcomeUser() //initial text popup that asks for the user's name (WIP)
 
 function drawNext() //function for updating the background according to player coordinates and direction
 {
-    let pC = player.getCoordinates();
+    pC = player.getCoordinates();
     $('#everything').css('background-image', 'url(' + tileArray[pC[0]][pC[1]].viewArray[pC[2]].src + ')');
+    getTileText(pC[0],pC[1]);
+}
+
+function getTileText(x, y) //checks if current tile has any text items to display via inspecting
+{
+    console.log(phase + '' + x + '' + y);
+    if(textCoordinates[phase + '' + x + '' + y]) //this depends entirely on which coordinates have a text item and under which phase
+    {
+        $('#inspect').css('visibility','visible'); //displaying the inspect button
+    }
+    else if ($('#inspect').is(':visible')) //turning off the inspect button if the coordinates/phase no longer match
+        $('#inspect').css('visibility','hidden');
+}
+
+function showText() //displays the text window in game
+{
+    //content in the json data is structured like "0_sound.ogg" where the digit is the index of the 
+    //text to display from 'texts.txt' and the .ogg a sound to play... _ is the splitting char
+    let textIndex = textCoordinates[phase + '' + pC[0] + '' + pC[1]].split("_")[0];
+    $('#textWindow p').text(texts[textIndex]); //displaying the pre-defined text according to the given index
+
+    $('#textWindow').css({top:550,position:'absolute',opacity:0,display:'block'}).
+    animate({top:'50%',opacity:1},600); //cool slide animation
+}
+
+function hideText() //hides the text window in game
+{
+    $('#textWindow').fadeOut();
 }
 
 function movePlayer(d) { //event handler for letting the player move forwards and backwards
@@ -101,7 +112,6 @@ function movePlayer(d) { //event handler for letting the player move forwards an
 }
 
 function turnPlayer(d){ //event handler for letting the player turn left and right
- 
     player.turn(d);
     drawNext();
 }
